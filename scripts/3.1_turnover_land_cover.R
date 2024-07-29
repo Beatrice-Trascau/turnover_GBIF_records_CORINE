ab2c70b68b8ab94ccef1670b58a8213ab634a56c
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2024-07-29 17:25:26 +0200

    Add code to convert occurrence records to sf with correct crs

--- scripts/3.1_turnover_land_cover.R
+++ scripts/3.1_turnover_land_cover.R
@@ -30,3 +30,23 @@ ssb_grids <- vect(here("data", "raw_data",
 # Cleaned occurrence records
 occurrences_norway <- fread(here("data", "derived_data", 
                                  "cleaned_occurrences_july24.txt"))
+
+# 2. PREPARE DATA FOR ANALYSIS -------------------------------------------------
+
+## 2.1. Raster with unique ID for each cell ------------------------------------
+
+# Create an ID raster with the same properties as CORINE
+land_cover_id <- norway_corine_status_modified_stack[[1]]
+
+#Assign each cell a unique ID from 1 to ncell
+land_cover_id[] <- 1:ncell(norway_corine_status_modified_stack[[1]])
+
+## 2.2. Occurrence records as sf -----------------------------------------------
+
+# Convert occurrence records df to sf object
+occurrences_sf <- st_as_sf(occurrences_norway,
+                           coords = c("decimalLongitude", "decimalLatitude"),
+                           crs = 4326)
+# Re-project 
+clean_occurrences_sf <- st_transform(clean_occurrences_sf, 
+                                     st_crs(norway_corine[[1]]))
