6d72fd59d8f046b0c2627eb26d8b5cdc23cea0af
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2024-10-24 13:49:24 +0200

    Save new rasters and convert to dfs

--- scripts/1.2_CORINE_aggregation.R
+++ scripts/1.2_CORINE_aggregation.R
@@ -60,7 +60,54 @@ corine_1km <- corine_list[[1]]
 corine_5km <- corine_list[[2]]
 corine_15km <- corine_list[[3]]
 
+# Write new rasters to file
+terra::writeRaster(corine_1km, here("data", "derived_data", "corine_1km.tif"))
+terra::writeRaster(corine_5km, here("data", "derived_data", "corine_5km.tif"))
+terra::writeRaster(corine_15km, here("data", "derived_data", "corine_15km.tif"))
+
+# 3. CREATE DF OF % CHANGES FOR AGGREGATIONS -----------------------------------
+
+## 3.1. 1km --------------------------------------------------------------------
+# Add change values to original df 
+corine_1km_df <- as.data.frame(corine_1km, xy = TRUE) |>
+  mutate(urban_change2000.2006 = (urban2006 - urban2000)/100,
+         complex_agri_change2000.2006 = (complex_agri2006 - complex_agri2000)/100,
+         agri_sig_veg_change2000.2006 = (agri_sig_veg2006 - agri_sig_veg2000)/100,
+         forest_change2000.2006 = (forest2006 - forest2000)/100,
+         moors_change2000.2006 = (moors2006 - moors2000)/100,
+         woodland_change2000.2006 = (woodland2006 - woodland2000)/100,
+         sparse_veg_change2000.2006 = (sparse_veg2006 - sparse_veg2000)/100,
+         urban_change2006.2012 = (urban2012 - urban2006)/100,
+         complex_agri_change2006.2012 = (complex_agri2012 - complex_agri2006)/100,
+         agri_sig_veg_change2006.2012 = (agri_sig_veg2012 - agri_sig_veg2006)/100,
+         forest_change2006.2012 = (forest2012 - forest2006)/100,
+         moors_change2006.2012 = (moors2012 - moors2006)/100,
+         woodland_change2006.2012 = (woodland2012 - woodland2006)/100,
+         sparse_veg_change2006.2012 = (sparse_veg2012 - sparse_veg2006)/100,
+         urban_change2012.2018 = (urban2018 - urban2012)/100,
+         complex_agri_change2012.2018 = (complex_agri2018 - complex_agri2012)/100,
+         agri_sig_veg_change2012.2018 = (agri_sig_veg2018 - agri_sig_veg2012)/100,
+         forest_change2012.2018 = (forest2018 - forest2012)/100,
+         moors_change2012.2018 = (moors2018 - moors2012)/100,
+         woodland_change2012.2018 = (woodland2018 - woodland2012)/100,
+         sparse_veg_change2012.2018 = (sparse_veg2018 - sparse_veg2012)/100)
 
 
-# 3. 
-corine_1km_df <- as.data.frame(corine_1km, xy = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
