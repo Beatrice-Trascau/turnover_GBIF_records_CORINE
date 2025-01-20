ec7f283aaa6a7eb803019bb9425210cc61e63122
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-01-20 19:24:22 +0100

    New script to add calculate land cover transitions for all spatial resolutions used

---
+++ scripts/3.1_land_cover_calculations.R
@@ -0,0 +1,51 @@
+##----------------------------------------------------------------------------##
+# PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS
+# 3.1_land_cover_calculations
+# This script contains code which calculates the land cover transitions in the
+# original rasters and the aggregations (1km, 5km, 15km)
+##----------------------------------------------------------------------------##
+
+# 1. LOAD DATA -----------------------------------------------------------------
+
+# CORINE Status Layers
+norway_corine_status_modified_stack <- rast(here("data", "derived_data",
+                                                 "norway_corine_status_modified_stack.tif"))
+
+# 2. CALCULATE LC CHANGES FOR 100M RESOLUTION ----------------------------------
+
+# Change name of layers 
+names(norway_corine_status_modified_stack) <- c("lc2000", "lc2006", 
+                                                "lc2012", "lc2018")
+
+## 2.1. Forest -> TWS ----------------------------------------------------------
+
+# 2000-2006
+transitions_2000_2006 <- analyze_forest_transition(norway_corine_status_modified_stack$"lc2000", 
+                                                   norway_corine_status_modified_stack$"lc2006")
+
+# 2006-2012
+transitions_2006_2012 <- analyze_forest_transition(norway_corine_status_modified_stack$"lc2006", 
+                                                   norway_corine_status_modified_stack$"lc2012")
+
+# 2012-2018
+transitions_2012_2018 <- analyze_forest_transition(norway_corine_status_modified_stack$"lc2012", 
+                                                   norway_corine_status_modified_stack$"lc2018")
+
+# Combine into single raster
+clc_100m_forest_tws <- c(transitions_2000_2006, transitions_2006_2012,
+                         transitions_2012_2018)
+
+# Set categories for layers (so that you know what each value represents)
+categories <- data.frame(
+  value = 0:3,
+  class = c("Non-forest", "Forest stable", 
+            "Forest to shrubland", "Other forest conversion"))
+
+# Change categories
+levels(clc_100m_forest_tws) <- categories
+
+# Write raster to file
+terra::writeRaster(clc_100m_forest_tws, 
+                   here("data", "derived_data", 
+                        "clc_status_100m_forest_tws.tif"), 
+                   overwrite = TRUE)
