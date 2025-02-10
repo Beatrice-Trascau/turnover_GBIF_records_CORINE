aa093d7ce03053974bf9cf6cb4970a6d4fd16019
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-02-10 10:03:35 +0100

    style: changed commenting of functions feat: removed functions no longer needed

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -5,7 +5,9 @@
 # functions used in the analysis
 ##----------------------------------------------------------------------------##
 
-# 1. FUNCTION TO LOAD/INSTALL PACKAGES NEEDED FOR ANALYIS ----------------------
+# 1. LOAD/INSTALL PACKAGES NEEDED FOR ANALYIS ----------------------------------
+
+# Function to check to install/load packages
 
 # Define function
 install_load_package <- function(x) {
@@ -24,12 +26,15 @@ package_vec <- c("here", "terra", "sf", "geodata", "mapview",
                  "tidyterra", "ggspatial", "htmlwidgets",
                  "htmltools", "patchwork", "webshot2",
                  "rgbif", "CoordinateCleaner", "codyn",
-                 "gratia", "lattice", "car") # specify packages
+                 "gratia", "lattice", "car")
 
 # Execute the function
 sapply(package_vec, install_load_package)
 
-# 2. FUNCTION TO ONLY DOWNLOAD FILES THAT ARE NOT ALREADY IN THE FOLDERS -------
+# 2. DOWNLOAD FILES ------------------------------------------------------------
+
+# Function to check if files are in directory and then download them if they
+# aren't
 
 download_files <- function(urls, filenames, dir = here("data", "raw_data")) {
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
@@ -41,7 +46,9 @@ download_files <- function(urls, filenames, dir = here("data", "raw_data")) {
   }
 }
 
-# 3. FUNCTION TO READ RASTERS --------------------------------------------------
+# 3. READ RASTERS --------------------------------------------------------------
+
+# Function to read rasters (from the raw_data file)
 
 read_rasters <- function(filenames, dir = here("data/raw_data")) {
   rasters <- lapply(filenames, function(x) {
@@ -54,13 +61,17 @@ read_rasters <- function(filenames, dir = here("data/raw_data")) {
   return(do.call(c, rasters))
 }
 
-# 4. FUNCTION TO CROP AND MASK RASTERS TO NORWAY -------------------------------
+# 4. CROP AND MASK RASTERS TO NORWAY -------------------------------------------
+
+# Function to crop and mask rasters to Norway
 
 crop_mask_to_norway <- function(raster_stack, norway_shape) {
   return(crop(raster_stack, norway_shape, mask = TRUE))
 }
 
-# 5. FUNCTION TO MODIFY CLASSES IN RASTERS -------------------------------------
+# 5. MODIFY CLASSES IN RASTERS -------------------------------------------------
+
+# Function to modify class values in the rasters
 
 modify_class_values <- function(raster_stack, class_modifications) {
   modified_stack <- raster_stack
@@ -73,130 +84,9 @@ modify_class_values <- function(raster_stack, class_modifications) {
   return(modified_stack)
 }
 
-# 6. FUNCTION TO COUNT THE NUMBER OF SMALL PIXELS WITH CERTAIN VALUES ----------
+# 6. CALCULATE FOREST -> TWS TRANSITIONS ---------------------------------------
 
-calculate_counts <- function(x) {
-  # remove NA values before counting
-  x <- na.omit(x)
-  
-  # count occurrences of each land cover category value
-  counts <- table(factor(x, levels = c(1, 80, 103, 250, 380, 590, 711)))
-  
-  # return the counts as a numeric vector
-  return(as.numeric(counts))
-}
-
-# 7. FUNCTION TO EXTRACT LAND COVER FOR GIVEN PERIOD ---------------------------
-
-extract_land_cover <- function(cell_ids, period) {
-  if (period == "1997-2000") {
-    land_cover <- terra::extract(norway_corine_status_modified_stack[[1]], cell_ids)[, 1]
-  } else if (period == "2003-2006") {
-    land_cover <- terra::extract(norway_corine_status_modified_stack[[2]], cell_ids)[, 1]
-  } else if (period == "2009-2012") {
-    land_cover <- terra::extract(norway_corine_status_modified_stack[[3]], cell_ids)[, 1]
-  } else {
-    land_cover <- NA
-  }
-  return(land_cover)
-}
-
-# 8. FUNCTION TO CALCULATE JACCARD DISSIMILARITY INDEX FOR PERIODS -------------
-
-calculate_jaccard_for_periods <- function(df, start_period, end_period) {
-  start_data <- df |>
-    filter(period == start_period)
-  end_data <- df |>
-    filter(period == end_period)
-  
-  combined_data <- start_data |>
-    full_join(end_data, by = "cell", suffix = c("_start", "_end"))
-  
-  # Extract land cover for start period
-  combined_data <- combined_data |>
-    mutate(land_cover_start = extract_land_cover(cell, start_period))
-  
-  jaccard_results <- combined_data |>
-    group_by(cell) |>
-    summarize(
-      species_start = list(unique(species_start)),
-      species_end = list(unique(species_end)),
-      jaccard_dissimilarity = 1 - length(intersect(species_start[[1]], 
-                                                   species_end[[1]])) / length(union(species_start[[1]], 
-                                                                                     species_end[[1]])),
-      land_cover_start = first(land_cover_start)) |>
-    mutate(start_period = start_period, end_period = end_period)
-  
-  return(jaccard_results)
-}
-
-# 8. FUNCTION TO FILTER CELLS WITH MORE THAN 3 SPECIES -------------------------
-
-# Filter cells with more than 3 species in every period (1km, 5km, 15km)
-filter_cells_with_species_each_period <- function(occurrences_df) {
-  # Count species per cell and period
-  species_count <- occurrences_df |>
-    st_drop_geometry() |>
-    group_by(cell, period) |>
-    summarise(species_count = n_distinct(species), .groups = 'drop')
-  
-  # Identify cells that have more than 3 species in every period
-  cells_with_more_than_3_species_each_period <- species_count |>
-    group_by(cell) |>
-    filter(all(species_count > 3)) |>
-    pull(cell) |>
-    unique()
-  
-  # Filter occurrences for cells with more than 3 species in every period
-  filtered_occurrences <- occurrences_df |>
-    filter(cell %in% cells_with_more_than_3_species_each_period) |>
-    st_drop_geometry() |>
-    select(period, species, cell) |>
-    na.omit()
-  
-  # Aggregate to ensure unique species records per cell and period
-  unique_occurrences <- filtered_occurrences |>
-    distinct(cell, species, period, .keep_all = TRUE)
-  
-  return(unique_occurrences)
-}
-
-# 9. FUNCTION TO CALCULATE JACCARDS' INDEX FOR EACH AGGREGATION AND PEROIOD ----
-
-calculate_turnover_for_resolutions <- function(unique_occurrences_df) {
-  # defined function to calculate Jaccard's dissimilarity index for each specified
-  # period combination for the given data frame of unique occurrences
-  jaccard_index_list <- lapply(period_combinations, function(periods) {
-    # apply function to each pair of periods in the list "period_combinations"
-    calculate_jaccard_for_periods(unique_occurrences_df, periods[1], periods[2])
-    # use custom function to calculate the index between the two periods
-  })
-  return(bind_rows(jaccard_index_list))
-}
-
-# 10. FUNCTION TO ADD CELL COORDS AND LAND COVER CATEGORY ----------------------
-
-# Function to add cell coordinates and land cover change values for each resolution
-add_cell_coordinates_and_land_cover <- function(temporal_turnover_df, land_cover_id_raster, land_cover_df) {
-  # Extract xy coordinates
-  xy_coords <- terra::xyFromCell(land_cover_id_raster, 1:ncell(land_cover_id_raster))
-  
-  # Create df of the xy coordinates
-  centroids_df <- data.frame(cell = 1:ncell(land_cover_id_raster), 
-                             x = xy_coords[,1], y = xy_coords[,2])
-  
-  # Merge coordinates with temporal turnover data
-  temporal_turnover_coords <- left_join(temporal_turnover_df, centroids_df, by = "cell")
-  
-  # Merge land cover change data with temporal turnover data
-  temporal_turnover_with_lc <- left_join(temporal_turnover_coords, land_cover_df, by = "cell")
-  
-  return(temporal_turnover_with_lc)
-}
-
-# 11. FUNCTION TO CALCULATE FOREST -> TWS TRANSITIONS IN RASTERS ---------------
-
-# Function to analyze forest transitions between two time periods
+# Function to calculate Forest -> TWS transitions between two time periods
 analyse_forest_transition <- function(rast_t1, rast_t2) {
   # Create transition raster
   # 0 = non-forest in t1
@@ -227,9 +117,9 @@ analyse_forest_transition <- function(rast_t1, rast_t2) {
 }
 
 
-# 12. FUNCTION TO CALCULATE TWS -> FORESTS TRANSITIONS IN RASTERS --------------
+# 7. CALCULATE TWS -> FORESTS TRANSITIONS --------------------------------------
 
-# Function to analyze TWS transitions between two time periods
+# Function to calculate TWS -> Forest transitions between two time periods
 analyse_tws_transition <- function(rast_t1, rast_t2) {
   # Create transition raster
   # 0 = non-TWS in t1
@@ -259,8 +149,9 @@ analyse_tws_transition <- function(rast_t1, rast_t2) {
   return(transition)
 }
 
-# 13. FUNCTION TO CALCULATE ALLL -> URBAN TRANSITIONS IN RASTERS ---------------
+# 8. CALCULATE ALLL -> URBAN TRANSITIONS ---------------------------------------
 
+# Function to calculate All -> Urban transitions between two time periods
 analyse_urban_conversion <- function(rast_t1, rast_t2) {
   # Create transition raster
   # 0 = already urban in t1
@@ -306,11 +197,11 @@ analyse_urban_conversion <- function(rast_t1, rast_t2) {
   return(transition)
 }
 
-# 14. FUNCTION TO AGGREGATE TRANSITIONS RASTERS TO HIGHER GRID SIZES -----------
+# 9. AGGREGATE RASTERS TO LARGER GRIDS -----------------------------------------
 
-# This function aggregates the transition rasters (created with the functions above) to larger grid sizes
+# This function aggregates the transition rasters (created with the functions 
+#   above) to larger grid sizes
 
-# Function to aggregate transition rasters and count occurrences
 aggregate_transitions <- function(transition_raster, factor) {
   # Create empty list to store aggregated count rasters
   agg_counts <- list()
