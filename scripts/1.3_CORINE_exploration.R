fc72aabdac6be7edf9fa0691c27ce31caef2691d
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-02-10 11:31:06 +0100

    feat: new script to get summary statistics for land cover changes

---
+++ scripts/1.3_CORINE_exploration.R
@@ -0,0 +1,12 @@
+##----------------------------------------------------------------------------##
+# PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS
+# 1.3_CORINE_exploration
+# This script contains code which explores land cover transitions for each
+# grain size considered
+##----------------------------------------------------------------------------##
+
+# 1. LOAD DATA -----------------------------------------------------------------
+
+# CORINE Status Layers
+norway_corine_status_modified_stack <- rast(here("data", "derived_data",
+                                                 "norway_corine_status_modified_stack.tif"))
