252b01e4773bec97e28019739652330f9c9a9636
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-02-10 11:44:42 +0100

    style: changed headings

--- scripts/1.3_CORINE_exploration.R
+++ scripts/1.3_CORINE_exploration.R
@@ -7,38 +7,64 @@
 
 # 1. LOAD DATA -----------------------------------------------------------------
 
-# 100m resolution
+## 1.1. 100m resolution --------------------------------------------------------
+
+# Forest -> TWS
 forest_tws_100m <- rast(here("data", "derived_data", 
                              "clc_status_100m_forest_tws.tif"))
+
+# TWS -> Forest
 tws_forest_100m <- rast(here("data", "derived_data", 
                              "clc_status_100m_tws_forest.tif"))
+
+# All -> Urban
 all_urban_100m <- rast(here("data", "derived_data", 
                             "clc_status_100m_all_urban.tif"))
 
-# 1km resolution
+## 1.2. 1km resolution ---------------------------------------------------------
+
+# Forest -> TWS
 forest_tws_1km <- rast(here("data", "derived_data", 
                             "clc_status_1km_forest_tws.tif"))
+
+# TWS -> Forest
 tws_forest_1km <- rast(here("data", "derived_data", 
                             "clc_status_1km_tws_forest.tif"))
+
+# All -> Urban
 all_urban_1km <- rast(here("data", "derived_data", 
                            "clc_status_1km_all_urban.tif"))
 
-# 5km resolution
+## 1.3. 5km resolution ---------------------------------------------------------
+
+# Forest -> TWS
 forest_tws_5km <- rast(here("data", "derived_data", 
                             "clc_status_5km_forest_tws.tif"))
+
+# TWS -> Forest
 tws_forest_5km <- rast(here("data", "derived_data", 
                             "clc_status_5km_tws_forest.tif"))
+
+# All -> Urban
 all_urban_5km <- rast(here("data", "derived_data", 
                            "clc_status_5km_all_urban.tif"))
 
-# 15km resolution
+## 1.4. 15km resolution --------------------------------------------------------
+
+# Forest -> TWS
 forest_tws_15km <- rast(here("data", "derived_data", 
                              "clc_status_15km_forest_tws.tif"))
+
+# TWS -> Forest
 tws_forest_15km <- rast(here("data", "derived_data", 
                              "clc_status_15km_tws_forest.tif"))
+
+# All -> Urban
 all_urban_15km <- rast(here("data", "derived_data", 
                             "clc_status_15km_all_urban.tif"))
 
 # Load Norway shapefile
 norway <- vect(here("data", "derived_data", "reprojected_norway_shapefile", 
                     "norway_corine_projection.shp"))
+
+# 2. EXTRACT SUMMARY TABLES ----------------------------------------------------
