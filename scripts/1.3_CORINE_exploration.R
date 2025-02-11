052f782ca206c43d5d79b933f636e3920f3d5d44
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-02-11 12:04:10 +0100

    feat: implement function to plot LC transitions for time periods for each aggregation

--- scripts/1.3_CORINE_exploration.R
+++ scripts/1.3_CORINE_exploration.R
@@ -142,3 +142,122 @@ ggsave(
   height = 12,
   dpi = 300)
 
+## 3.2. Plot at 1km resolution -------------------------------------------------
+
+# Forest -> TWS
+p1_1km <- create_aggregated_panel(forest_tws_1km[[1]], norway, 
+                                  "Forest to TWS", "2000-2006", TRUE, TRUE)
+p2_1km <- create_aggregated_panel(forest_tws_1km[[2]], norway, 
+                                  "Forest to TWS", "2006-2012", FALSE, FALSE)
+p3_1km <- create_aggregated_panel(forest_tws_1km[[3]], norway, 
+                                  "Forest to TWS", "2012-2018", FALSE, FALSE)
+
+# TWS -> Forest
+p4_1km <- create_aggregated_panel(tws_forest_1km[[1]], norway, 
+                                  "TWS to Forest", "2000-2006", TRUE, FALSE)
+p5_1km <- create_aggregated_panel(tws_forest_1km[[2]], norway, 
+                                  "TWS to Forest", "2006-2012", FALSE, FALSE)
+p6_1km <- create_aggregated_panel(tws_forest_1km[[3]], norway, 
+                                  "TWS to Forest", "2012-2018", FALSE, FALSE)
+
+# All -> Urban panels
+p7_1km <- create_aggregated_panel(all_urban_1km[[1]], norway, 
+                                  "All to Urban", "2000-2006", TRUE, FALSE)
+p8_1km <- create_aggregated_panel(all_urban_1km[[2]], norway, 
+                                  "All to Urban", "2006-2012", FALSE, FALSE)
+p9_1km <- create_aggregated_panel(all_urban_1km[[3]], norway, "
+                                  All to Urban", "2012-2018", FALSE, FALSE)
+
+# Combine 1km plots into specific rows
+top_row_1km <- plot_grid(p1_1km, p4_1km, p7_1km, nrow = 1, align = 'h', 
+                         rel_widths = c(1.2, 1, 1))
+middle_row_1km <- plot_grid(p2_1km, p5_1km, p8_1km, nrow = 1, align = 'h')
+bottom_row_1km <- plot_grid(p3_1km, p6_1km, p9_1km, nrow = 1, align = 'h')
+
+# Combine all rows into 1
+final_plot_1km <- plot_grid(top_row_1km, middle_row_1km, bottom_row_1km,
+                            nrow = 3, align = 'v')
+
+# Save 1km plot
+ggsave(filename = here("figures", "Figure1_landcover_transitions_1km.png"),
+       plot = final_plot_1km, width = 12, height = 12, dpi = 300)
+
+## 3.3. Plot at 5km resolution -------------------------------------------------
+
+# Forest -> TWS
+p1_5km <- create_aggregated_panel(forest_tws_5km[[1]], norway, 
+                                  "Forest to TWS", "2000-2006", TRUE, TRUE)
+p2_5km <- create_aggregated_panel(forest_tws_5km[[2]], norway, 
+                                  "Forest to TWS", "2006-2012", FALSE, FALSE)
+p3_5km <- create_aggregated_panel(forest_tws_5km[[3]], norway, 
+                                  "Forest to TWS", "2012-2018", FALSE, FALSE)
+
+# TWS -> Forest
+p4_5km <- create_aggregated_panel(tws_forest_5km[[1]], norway, 
+                                  "TWS to Forest", "2000-2006", TRUE, FALSE)
+p5_5km <- create_aggregated_panel(tws_forest_5km[[2]], norway, 
+                                  "TWS to Forest", "2006-2012", FALSE, FALSE)
+p6_5km <- create_aggregated_panel(tws_forest_5km[[3]], norway, 
+                                  "TWS to Forest", "2012-2018", FALSE, FALSE)
+
+# All -> Urban 
+p7_5km <- create_aggregated_panel(all_urban_5km[[1]], norway, 
+                                  "All to Urban", "2000-2006", TRUE, FALSE)
+p8_5km <- create_aggregated_panel(all_urban_5km[[2]], norway, 
+                                  "All to Urban", "2006-2012", FALSE, FALSE)
+p9_5km <- create_aggregated_panel(all_urban_5km[[3]], norway, 
+                                  "All to Urban", "2012-2018", FALSE, FALSE)
+
+# Combine 5km plots into specifc rows
+top_row_5km <- plot_grid(p1_5km, p4_5km, p7_5km, nrow = 1, align = 'h',
+                         rel_widths = c(1.2, 1, 1))
+middle_row_5km <- plot_grid(p2_5km, p5_5km, p8_5km, nrow = 1, align = 'h')
+bottom_row_5km <- plot_grid(p3_5km, p6_5km, p9_5km, nrow = 1, align = 'h')
+
+# Combine all rows into one
+final_plot_5km <- plot_grid(top_row_5km, middle_row_5km, bottom_row_5km,
+                            nrow = 3, align = 'v')
+
+# Save 5km plot
+ggsave(filename = here("figures", "Figure1_landcover_transitions_5km.png"),
+       plot = final_plot_5km, width = 12, height = 12, dpi = 300)
+
+## 3.4. Plot at 15km resolution ------------------------------------------------
+
+# Forest -> TWS 
+p1_15km <- create_aggregated_panel(forest_tws_15km[[1]], norway, 
+                                   "Forest to TWS", "2000-2006", TRUE, TRUE)
+p2_15km <- create_aggregated_panel(forest_tws_15km[[2]], norway, 
+                                   "Forest to TWS", "2006-2012", FALSE, FALSE)
+p3_15km <- create_aggregated_panel(forest_tws_15km[[3]], norway,
+                                   "Forest to TWS", "2012-2018", FALSE, FALSE)
+
+# TWS -> Forest
+p4_15km <- create_aggregated_panel(tws_forest_15km[[1]], norway, 
+                                   "TWS to Forest", "2000-2006", TRUE, FALSE)
+p5_15km <- create_aggregated_panel(tws_forest_15km[[2]], norway, 
+                                   "TWS to Forest", "2006-2012", FALSE, FALSE)
+p6_15km <- create_aggregated_panel(tws_forest_15km[[3]], norway, 
+                                   "TWS to Forest", "2012-2018", FALSE, FALSE)
+
+# All -> Urban 
+p7_15km <- create_aggregated_panel(all_urban_15km[[1]], norway, 
+                                   "All to Urban", "2000-2006", TRUE, FALSE)
+p8_15km <- create_aggregated_panel(all_urban_15km[[2]], norway, 
+                                   "All to Urban", "2006-2012", FALSE, FALSE)
+p9_15km <- create_aggregated_panel(all_urban_15km[[3]], norway, 
+                                   "All to Urban", "2012-2018", FALSE, FALSE)
+
+# Combine 15km plots into specific rows
+top_row_15km <- plot_grid(p1_15km, p4_15km, p7_15km, nrow = 1, align = 'h',
+                          rel_widths = c(1.2, 1, 1))
+middle_row_15km <- plot_grid(p2_15km, p5_15km, p8_15km, nrow = 1, align = 'h')
+bottom_row_15km <- plot_grid(p3_15km, p6_15km, p9_15km, nrow = 1, align = 'h')
+
+# Combine all rows into 1
+final_plot_15km <- plot_grid(top_row_15km, middle_row_15km, bottom_row_15km,
+                             nrow = 3, align = 'v')
+
+# Save 15km plot
+ggsave(filename = here("figures", "Figure1_landcover_transitions_15km.png"),
+       plot = final_plot_15km, width = 12, height = 12, dpi = 300)
