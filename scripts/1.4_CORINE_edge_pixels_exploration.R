2d04df052d79e3a610e2eca76cfe51ce4f7eb402
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-10 13:16:16 +0100

    feat: calcualte % inside and outside of the boundary

--- scripts/1.4_CORINE_edge_pixels_exploration.R
+++ scripts/1.4_CORINE_edge_pixels_exploration.R
@@ -24,3 +24,28 @@ template_raster <- forest_tws_15km[[1]]
 # This will help us in the next step to differentiate target pixels from those that are NA or 0
 template_raster[] <- 1
 
+# Convert cells to polygons
+cell_polygons <- as.polygons(template_raster)
+
+# Convert polygons to sf object
+cell_polygons_sf <- st_as_sf(cell_polygons)
+
+# Convert shapefile to sf object
+norway_sf <- st_as_sf(norway)
+
+# Calculate area of each cell
+cell_polygons_sf$total_area <- st_area(cell_polygons_sf)
+
+# Intersect cells with boundary to get proportion of cells within Norway
+cells_within <- st_intersection(cell_polygons_sf, norway_sf)
+
+# Calculate area of the cells within
+cells_within$area_within <- st_area(cells_within)
+
+# Calculate % of each cell inside the country
+cells_within$percent_within <- as.numeric(cells_within$area_within/cells_within$total_area) * 100
+
+# Calculate % of each cell outside the country
+cells_within$percent_outside <- 100 - cells_within$percent_within
+
+# Extract cell coordinates from the centroids
