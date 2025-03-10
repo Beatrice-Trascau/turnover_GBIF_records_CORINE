52590d30189f9c05db6fb531e752857375d7a0e8
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-10 11:46:22 +0100

    feat: new script to explore the pixels falling outside of the Norway boundary

---
+++ scripts/1.4_CORINE_edge_pixels_exploration.R
@@ -0,0 +1,6 @@
+##----------------------------------------------------------------------------##
+# PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS
+# 1.4_CORINE_edge_pixels_exploration
+# This script contains code which identifies the pixels in the 15km rasters
+# which fall partially outside of the Norway boundary
+##----------------------------------------------------------------------------##
