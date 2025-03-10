d43f733385a55eca209523c364f8435956035e4e
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-10 11:49:19 +0100

    feat: read in data

--- scripts/1.4_CORINE_edge_pixels_exploration.R
+++ scripts/1.4_CORINE_edge_pixels_exploration.R
@@ -4,3 +4,13 @@
 # This script contains code which identifies the pixels in the 15km rasters
 # which fall partially outside of the Norway boundary
 ##----------------------------------------------------------------------------##
+
+# 1. LOAD DATA -----------------------------------------------------------------
+
+# One of the 15km rasters
+forest_tws_15km <- rast(here("data", "derived_data", 
+                             "clc_status_15km_forest_tws.tif"))
+
+# Norway shapefile
+norway <- vex(here("data", "derived_data", "reprojected_norway_shapefile",
+                   "norway_corine_projection.shp"))
