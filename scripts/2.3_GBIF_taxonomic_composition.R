e985430dd225f1fcbd6d04eb1fc43fb3c4a54623
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-11 17:24:17 +0100

    feat: added validation checks

--- scripts/2.3_GBIF_taxonomic_composition.R
+++ scripts/2.3_GBIF_taxonomic_composition.R
@@ -50,7 +50,7 @@ taxonomic_composition_periods <- bind_rows(period1_occurrences,
                                            period2_occurrences,
                                            period3_occurrences)
 
-## 4.3. Calculate counts and proportions by period and taxonomic group ---------
+## 2.3. Calculate counts and proportions by period and taxonomic group ---------
 taxonomic_summary <- taxonomic_composition_periods |>
   filter(!is.na(period)) |>
   group_by(period, taxonomic_group) |>
@@ -59,7 +59,7 @@ taxonomic_summary <- taxonomic_composition_periods |>
   mutate(total = sum(count), proportion = count / total) |>
   ungroup()
 
-# 5. CREATE STACKED PLOT -------------------------------------------------------
+# 3. CREATE STACKED PLOT -------------------------------------------------------
 
 # Create a colour pallete
 taxonomic_colours <- c("Plants" = "#66C2A5",
@@ -93,3 +93,67 @@ ggsave(filename = here("figures", "Figure2_taxonomic_composition_across_periods.
 # Save figure as .svg
 ggsave(filename = here("figures", "Figure2_taxonomic_composition_across_periods.svg"),
        width = 10, height = 8, dpi = 300)
+
+# 4. VALIDATION CHECKS ---------------------------------------------------------
+
+## 4.1. Check that period assignment was correct -------------------------------
+
+# Count records by year and assigned period to verify correct assignment
+year_period_check <- taxonomic_composition_periods |>
+  group_by(year, period) |>
+  summarise(count = n(), .groupd = "drop") |>
+  arrange(year)
+
+# View checks
+print(year_period_check, n= 25) #looks correct
+
+## 4.2. Check taxonomic group assignment ---------------------------------------
+
+# Count records by kingdom/class/phylum and assigned taxonomic group
+taxonomic_group_check <- taxonomic_composition_periods |>
+  group_by(kingdom, class, phylum, taxonomic_group) |>
+  summarise(count = n(), .groups = "drop") |>
+  arrange(taxonomic_group)
+
+# View checks
+print(taxonomic_group_check, n = 130) #looks ok
+
+## 4.3. Check total for each period --------------------------------------------
+
+# Calculate total records per period
+period_totals <- taxonomic_composition_periods |>
+  group_by(period) |>
+  summarise(total_records = n(), .groups = "drop")
+
+# View checks
+print(period_totals)
+
+## 4.4. Check proportions add up to 1 for each period --------------------------
+
+# Verify that proportions sum to 1 (or close)
+proportion_check <- taxonomic_summary |>
+  group_by(period) |>
+  summarise(sum_proportion = sum(proportion), .group = "drop")
+
+# View check
+print(proportion_check) # all add up to 1
+
+## 4.5. Cross-check with other methods -----------------------------------------
+
+# Get only birds for 2012-2015
+bird_2012_2015 <- taxonomic_composition_periods |>
+  filter(taxonomic_group == "Birds" & period == "2012-2015")
+
+# Check how many occurrences for birds in 2012-2015
+nrow(bird_2012_2015) #4 654 633
+
+# Check how many bird records from 2012-2015 there are in the summary df
+summary_bird_2012_2015 <- taxonomic_summary |>
+  filter(taxonomic_group == "Birds" & period == "2012-2015")
+
+# Print total value from the summary df
+print(summary_bird_2012_2015$count) # 4 654 633
+
+# They are matching! Can try this with more combinations of years and groups.
+
+# END OF SCRIPT ----------------------------------------------------------------
