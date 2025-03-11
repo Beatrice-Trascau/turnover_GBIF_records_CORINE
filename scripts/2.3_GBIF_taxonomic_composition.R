5c5d4936c928f65acf7e124d2f9620ecd1b3ff91
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-11 17:04:07 +0100

    feat: plot stacked barplot of number of occurrences by group and period

--- scripts/2.3_GBIF_taxonomic_composition.R
+++ scripts/2.3_GBIF_taxonomic_composition.R
@@ -46,6 +46,50 @@ period3_occurrences <- taxonomic_composition |>
                             year %in% 2015:2018 ~ "2015-2018"))
 
 # Combine all periods
-occurrences_taxonomy <- bind_rows(period1_occurrences,
-                                  period2_occurrences,
-                                  period3_occurrences)
+taxonomic_composition_periods <- bind_rows(period1_occurrences,
+                                           period2_occurrences,
+                                           period3_occurrences)
+
+## 4.3. Calculate counts and proportions by period and taxonomic group ---------
+taxonomic_summary <- taxonomic_composition_periods |>
+  filter(!is.na(period)) |>
+  group_by(period, taxonomic_group) |>
+  summarise(count = n(), .groups = "drop") |>
+  group_by(period) |>
+  mutate(total = sum(count), proportion = count / total) |>
+  ungroup()
+
+# 5. CREATE STACKED PLOT -------------------------------------------------------
+
+# Create a colour pallete
+taxonomic_colours <- c("Plants" = "#66C2A5",
+                     "Birds" = "#FC8D62",
+                     "Arthropods" = "#8DA0CB",
+                     "Mammals" = "#E78AC3",
+                     "Other" = "#A6D854")
+
+# Define order for the periods
+period_order <- c("1997-2000", "2006-2009", "2003-2006", 
+                  "2012-2015", "2008-2012", "2015-2018")
+
+# Convert period column to a factor and follow the order
+taxonomic_summary <- taxonomic_summary |>
+  mutate(period = factor(period, levels = period_order))
+
+# Create stacked barplot
+taxonomic_plot <- ggplot(taxonomic_summary,
+                         aes(x = period, y = proportion, fill = taxonomic_group)) +
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_manual(values = taxonomic_colours) +
+  labs(x = "Time Period", y = "Proportion", fill = "Group") +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right")
+
+# Save figure as .png
+ggsave(filename = here("figures", "Figure2_taxonomic_composition_across_periods.png"),
+       width = 10, height = 8, dpi = 300)
+
+# Save figure as .svg
+ggsave(filename = here("figures", "Figure2_taxonomic_composition_across_periods.svg"),
+       width = 10, height = 8, dpi = 300)
