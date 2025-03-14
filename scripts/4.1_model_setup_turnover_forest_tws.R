08446fdd7d46ed97c2975a42da103f61992c72d6
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-14 11:33:11 +0100

    feat: prepare df for model

--- scripts/4.1_model_setup_turnover_forest_tws.R
+++ scripts/4.1_model_setup_turnover_forest_tws.R
@@ -4,3 +4,52 @@
 # This script contains code which sets up the models exploring the imapct of 
 # Forest -> TWS land cover transition on temporal turnover
 ##----------------------------------------------------------------------------##
+
+# 1. LOAD DATA -----------------------------------------------------------------
+load(here("data", "derived_data", 
+          "all_periods_turnover_all_land_cover_chanegs_15km.rda"))
+
+# 2. PREPARE DATA FOR ANALYSIS -------------------------------------------------
+
+## 2.1. Select only Forest -> TWS columns --------------------------------------
+
+# Check column names
+colnames(all_periods_turnover_all_land_cover_chanegs_15km)
+
+# Also rename the columns for easier manipulation of df
+turnover_forest_tws_15km <- all_periods_turnover_all_land_cover_chanegs_15km |>
+  select(-c('2000-2006_TWS no change', '2000-2006_TWS to Forest',
+            '2000-2006_Urban_no_change', '2000-2006_all_to_urban',
+            '2006-2012_TWS no change', '2006-2012_TWS to Forest',
+            '2006-2012_Urban_no_change', '2006-2012_all_to_urban',
+            '2012-2018_TWS no change', '2012-2018_TWS to Forest',
+            '2012-2018_Urban_no_change', '2012-2018_all_to_urban')) |>
+  # determine which rows belong to which time period
+  mutate(forest_no_change = case_when(lc_time_period == "2000-2006" ~ `2000-2006_Forest no change`,
+                                      lc_time_period == "2006-2012" ~ `2006-2012_Forest no change`,
+                                      lc_time_period == "2012-2018" ~ `2012-2018_Forest no change`,
+                                      TRUE ~ NA_real_),
+         forest_to_tws = case_when(lc_time_period == "2000-2006" ~ `2000-2006_Forest to TWS`,
+                                   lc_time_period == "2006-2012" ~ `2006-2012_Forest to TWS`,
+                                   lc_time_period == "2012-2018" ~ `2012-2018_Forest to TWS`,
+                                   TRUE ~ NA_real_)) |>
+  # remove columns no longer required
+  select(-`2000-2006_Forest no change`, -`2006-2012_Forest no change`, 
+         -`2012-2018_Forest no change`,-`2000-2006_Forest to TWS`, 
+         -`2006-2012_Forest to TWS`, -`2012-2018_Forest to TWS`)
+
+# Check transformation went ok
+head(turnover_forest_tws_15km)
+
+## 2.2. Transform JDI values for beta regression -------------------------------
+
+# JDI is in [0,1] but beta regression requires that values do not touch 0 and 1
+# so we will transform the JDI values according to this formula: 
+# ( Y * (N - 1) + 0.5 ) / N, Y = response variable, N = sample size
+
+# Get N
+N <- nrow(turnover_forest_tws_15km)
+
+# Calculate new JDI values
+turnover_forest_tws_15km <- turnover_forest_tws_15km |>
+  mutate(JDI_beta = (JDI * (N - 1) + 0.5) / N)
