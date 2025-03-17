b18d965106fc10fcde8cd5d6c3699e317cd7cb0c
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-17 11:16:45 +0100

    chore: rename script

--- scripts/4.3_model_setup_turnover_tws_forest.R
+++ scripts/4.3_model_setup_turnover_tws_forest.R
@@ -1,6 +1,6 @@
 ##----------------------------------------------------------------------------##
 # PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS
-# 4.2_model_setup_turnover_tws_forest
+# 4.3_model_setup_turnover_tws_forest
 # This script contains code which sets up the models exploring the imapct of 
 # Forest -> TWS land cover transition on temporal turnover
 ##----------------------------------------------------------------------------##
