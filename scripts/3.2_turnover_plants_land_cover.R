f2dfc7cff9b9a80b1cb11dce7f22b4bd3c19fb25
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-05-05 18:47:21 +0200

    feat: added code to extract land cover values, calculate turnover and save final results

--- scripts/3.2_turnover_plants_land_cover.R
+++ scripts/3.2_turnover_plants_land_cover.R
@@ -1,6 +1,6 @@
 ##----------------------------------------------------------------------------##
 # PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS
-# 3.1_turnover_plants_land_cover
+# 3.2_turnover_plants_land_cover
 # This script contains code which calculates the temporal turnover of GBIF
 # vascular plant records in CORINE land cover pixels
 ##----------------------------------------------------------------------------##
@@ -248,3 +248,254 @@ cell_2012_2018_summary_wide <- cell_2012_2018_summary_wide |>
                                       function(x) if(is.null(x)) character(0) else x),
          species_list_after = lapply(species_list_after, 
                                      function(x) if(is.null(x)) character(0) else x))
+
+# 6. GET LC VALUES -------------------------------------------------------------
+
+## 6.1. First period: 2000-2006 ------------------------------------------------
+
+# Check which layers are needed
+names(forest_tws_15km) #[[2]] = "2000-2006_Forest no change", [[3]] = "2000-2006_Forest to TWS"
+names(tws_forest_15km) #[[2]] = "2000-2006_TWS no change", [[3]] = "2000-2006_TWS to Forest"
+names(all_urban_15km) #[[1]] = "2000-2006_Urban_no_change", [[2]] = "2000-2006_all_to_urban"
+
+### 6.1.1. Forests -> TWS ------------------------------------------------------
+
+# Create df with raster layers of interest
+forest_tws_df <- as.data.frame(forest_tws_15km[[c(2,3)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+forest_tws_df$cell_ID <- terra::extract(reference_grid,
+                                        forest_tws_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+forest_tws_df <- forest_tws_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2000_2006_summary_wide_LC1 <- cell_2000_2006_summary_wide |>
+  left_join(forest_tws_df, by = "cell_ID")
+
+### 6.1.2. TWS -> Forests ------------------------------------------------------
+
+# Create df with raster values
+tws_forest_df <- as.data.frame(tws_forest_15km[[c(2,3)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+tws_forest_df$cell_ID <- terra::extract(reference_grid,
+                                        tws_forest_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+tws_forest_df <- tws_forest_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2000_2006_summary_wide_LC2 <- cell_2000_2006_summary_wide_LC1 |>
+  left_join(tws_forest_df |>
+              select(cell_ID, '2000-2006_TWS no change', '2000-2006_TWS to Forest'),
+            by = "cell_ID")
+
+### 6.1.3. All -> Urban --------------------------------------------------------
+
+# Create df with raster values
+all_urban_df <- as.data.frame(all_urban_15km[[c(1,2)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+all_urban_df$cell_ID <- terra::extract(reference_grid,
+                                       all_urban_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+all_urban_df <- all_urban_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2000_2006_summary_wide_LC3 <- cell_2000_2006_summary_wide_LC2 |>
+  left_join(all_urban_df |>
+              select(cell_ID, '2000-2006_Urban_no_change', '2000-2006_all_to_urban'),
+            by = "cell_ID")
+
+## 6.2. Second period: 2006-2012 -----------------------------------------------
+
+# Check which layers are needed
+names(forest_tws_15km) #[[6]] = "2006-2012_Forest no change", [[7]] = "2006-2012_Forest to TWS"
+names(tws_forest_15km) #[[6]] = "2006-2012_TWS no change", [[7]] = "2006-2012_TWS to Forest"
+names(all_urban_15km) #[[4]] = "2006-2012_Urban_no_change", [[5]] = "2006-2012_all_to_urban"
+
+### 6.2.1. Forests -> TWS ------------------------------------------------------
+
+# Create df with raster layers of interest
+forest_tws_2006_2012_df <- as.data.frame(forest_tws_15km[[c(6,7)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+forest_tws_2006_2012_df$cell_ID <- terra::extract(reference_grid,
+                                                  forest_tws_2006_2012_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+forest_tws_2006_2012_df <- forest_tws_2006_2012_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2006_2012_summary_wide_LC1 <- cell_2006_2012_summary_wide |>
+  left_join(forest_tws_2006_2012_df, by = "cell_ID")
+
+### 6.2.2. TWS -> Forests ------------------------------------------------------
+
+# Create df with raster values
+tws_forest_2006_2012_df <- as.data.frame(tws_forest_15km[[c(6,7)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+tws_forest_2006_2012_df$cell_ID <- terra::extract(reference_grid,
+                                                  tws_forest_2006_2012_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+tws_forest_2006_2012_df <- tws_forest_2006_2012_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2006_2012_summary_wide_LC2 <- cell_2006_2012_summary_wide_LC1 |>
+  left_join(tws_forest_2006_2012_df |>
+              select(cell_ID, '2006-2012_TWS no change', '2006-2012_TWS to Forest'),
+            by = "cell_ID")
+
+### 6.2.3. All -> Urban --------------------------------------------------------
+
+# Create df with raster values
+all_urban_2006_2012_df <- as.data.frame(all_urban_15km[[c(4,5)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+all_urban_2006_2012_df$cell_ID <- terra::extract(reference_grid,
+                                                 all_urban_2006_2012_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+all_urban_2006_2012_df <- all_urban_2006_2012_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2006_2012_summary_wide_LC3 <- cell_2006_2012_summary_wide_LC2 |>
+  left_join(all_urban_2006_2012_df |>
+              select(cell_ID, '2006-2012_Urban_no_change', '2006-2012_all_to_urban'),
+            by = "cell_ID")
+
+
+## 6.3. Third period: 2012-2018 ------------------------------------------------
+
+# Check which layers are needed
+names(forest_tws_15km) #[[10]] = "2012-2018_Forest no change", [[11]] = "2012-2018_Forest to TWS"
+names(tws_forest_15km) #[[10]] = "2012-2018_TWS no change", [[11]] = "2012-2018_TWS to Forest"
+names(all_urban_15km) #[[7]] = "2012-2018_Urban_no_change", [[8]] = "2012-2018_all_to_urban"
+
+
+### 6.3.1. Forests -> TWS ------------------------------------------------------
+
+# Create df with raster layers of interest
+forest_tws_2012_2018_df <- as.data.frame(forest_tws_15km[[c(10,11)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+forest_tws_2012_2018_df$cell_ID <- terra::extract(reference_grid,
+                                                  forest_tws_2012_2018_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+forest_tws_2012_2018_df <- forest_tws_2012_2018_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2012_2018_summary_wide_LC1 <- cell_2012_2018_summary_wide |>
+  left_join(forest_tws_2012_2018_df, by = "cell_ID")
+
+### 6.3.2. TWS -> Forests ------------------------------------------------------
+
+# Create df with raster values
+tws_forest_2012_2018_df <- as.data.frame(tws_forest_15km[[c(10,11)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+tws_forest_2012_2018_df$cell_ID <- terra::extract(reference_grid,
+                                                  tws_forest_2012_2018_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+tws_forest_2012_2018_df <- tws_forest_2012_2018_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2012_2018_summary_wide_LC2 <- cell_2012_2018_summary_wide_LC1 |>
+  left_join(tws_forest_2012_2018_df |>
+              select(cell_ID, '2012-2018_TWS no change', '2012-2018_TWS to Forest'),
+            by = "cell_ID")
+
+### 6.3.3. All -> Urban --------------------------------------------------------
+
+# Create df with raster values
+all_urban_2012_2018_df <- as.data.frame(all_urban_15km[[c(7,8)]], xy = TRUE)
+
+# Extract cell IDs for each coordinate pair
+all_urban_2012_2018_df$cell_ID <- terra::extract(reference_grid,
+                                                 all_urban_2012_2018_df[, c("x", "y")])[, "cell_id"]
+
+# Remove rows with NA cell_ID
+all_urban_2012_2018_df <- all_urban_2012_2018_df |>
+  filter(!is.na(cell_ID))
+
+# Merge these values with the occurrences df
+cell_2012_2018_summary_wide_LC3 <- cell_2012_2018_summary_wide_LC2 |>
+  left_join(all_urban_2012_2018_df |>
+              select(cell_ID, '2012-2018_Urban_no_change', '2012-2018_all_to_urban'),
+            by = "cell_ID")
+
+# 7. CALCULATE JACCARD'S DISSIMILARITY INDEX -----------------------------------
+
+## 7.1. First period: 2000-2006 ------------------------------------------------
+
+# Filter cells with < 3 species in each time step
+cell_2000_2006_filtered <- cell_2000_2006_summary_wide_LC3 |>
+  filter(total_spp_before >= 3 & total_spp_after >= 3)
+
+# Calculate Jaccard's Dissimilarity Index
+turnover_2000_2006_lc <- cell_2000_2006_filtered |>
+  rowwise() |>
+  # calculate shared (intersection) and total (union) species
+  mutate(intersection_size = length(intersect(species_list_before, species_list_after)),
+         union_size = length(union(species_list_before, species_list_after)),
+         JDI = 1 - (intersection_size / union_size)) |>
+  ungroup()
+
+## 7.2. Second period: 2006-2012 -----------------------------------------------
+
+# Filter cells with < 3 species in each time step
+cell_2006_2012_filtered <- cell_2006_2012_summary_wide_LC3 |>
+  filter(total_spp_before >= 3 & total_spp_after >= 3)
+
+# Calculate Jaccard's Dissimilarity Index
+turnover_2006_2012_lc <- cell_2006_2012_filtered |>
+  rowwise() |>
+  # calculate shared (intersection) and total (union) species
+  mutate(intersection_size = length(intersect(species_list_before, species_list_after)),
+         union_size = length(union(species_list_before, species_list_after)),
+         JDI = 1 - (intersection_size / union_size)) |>
+  ungroup()
+
+## 7.3. Third period: 2012-2018 ------------------------------------------------
+
+# Filter cells with < 3 species in each time step
+cell_2012_2018_filtered <- cell_2012_2018_summary_wide_LC3 |>
+  filter(total_spp_before >= 3 & total_spp_after >= 3)
+
+# Calculate Jaccard's Dissimilarity Index
+turnover_2012_2018_lc <- cell_2012_2018_filtered |>
+  rowwise() |>
+  # calculate shared (intersection) and total (union) species
+  mutate(intersection_size = length(intersect(species_list_before, species_list_after)),
+         union_size = length(union(species_list_before, species_list_after)),
+         JDI = 1 - (intersection_size / union_size)) |>
+  ungroup()
+
+## 7.4 Combine all into single df ----------------------------------------------
+
+# Combine all periods into a single dataframe ----------------------------------
+vascular_plants_all_periods_turnover_all_land_cover_chanegs_15km <- bind_rows(turnover_2000_2006_lc,
+                                                              turnover_2006_2012_lc,
+                                                              turnover_2012_2018_lc)
+
+# Save df to file
+save(vascular_plants_all_periods_turnover_all_land_cover_chanegs_15km,
+     file = here("data", "derived_data", 
+                 "vascular_plants_all_periods_turnover_all_land_cover_chanegs_15km.rda"))
+
+# END OF SCRIPT ----------------------------------------------------------------
