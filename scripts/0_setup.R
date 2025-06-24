74fd7e773dc3e29bfb2e058b728521e3d7e24851
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-06-24 10:26:25 +0200

    feat: added more packages to load

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -27,7 +27,8 @@ package_vec <- c("here", "terra", "sf", "geodata", "mapview",
                  "htmltools", "patchwork", "webshot2",
                  "rgbif", "CoordinateCleaner", "codyn",
                  "gratia", "lattice", "car", "kableExtra",
-                 "betareg", "spdep", "corrplot")
+                 "betareg", "spdep", "corrplot", "leaflet", "
+                 htmlwidgets", "viridis", "plotly", "DT")
 
 # Execute the function
 sapply(package_vec, install_load_package)
