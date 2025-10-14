a35c6442ca4308c1a52b7ba6f520a68ccecda7d4
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-10-14 11:50:50 +0200

    feat: add number of occurrences removed chore: remove data filering steps for smaller grains

--- scripts/2.2_GBIF_data_cleaning.R
+++ scripts/2.2_GBIF_data_cleaning.R
@@ -7,34 +7,49 @@
 # 1. LOAD DATA -----------------------------------------------------------------
 
 # Load data
-load(here("data", "raw_data", "occurrence_a.rda"))
+load(here("data","raw_data","occurrence_redownload_4August2025.txt"))
 
 # 2. CLEAN RECORDS -------------------------------------------------------------
 
+# Original number of occurrences in raw dataframe: 
+nrow(occurrence) # 29 471 774
+
 # 2.1. Remove material citations, fossil specimens, and living specimens -------
 
-clean_occurrences1 <- occurrence_a |>
+clean_occurrences1 <- occurrence |>
   filter(!basisOfRecord %in% c("MATERIAL_CITATION", "FOSSIL_SPECIMEN",
                                "LIVING_SPECIMEN"))
+
+# Occurrences left after material citations, fossil specimens and living specimens removed
+nrow(clean_occurrences1) # 29 380 790
 
 # 2.2. Remove records that are not Animalia, Plantae or Fungi ------------------
 
 clean_occurrences2 <- clean_occurrences1 |>
   filter(kingdom %in% c("Animalia", "Plantae", "Fungi"))
 
+# Check how many records are left
+nrow(clean_occurrences2) # 29 129 192
+
 # 2.3. Remove records with no registered species-level information -------------
 clean_occurrences3 <- clean_occurrences2 |>
   filter(specificEpithet != "")
+
+# Check how many records are left
+nrow(clean_occurrences3) # 28 269 295
 
 # 2.4. Remove duplicate records ------------------------------------------------
 clean_occurrences4 <- clean_occurrences3 |>
   distinct()
 
+# Check how many records are left
+nrow(clean_occurrences4) # 28 269 295 -> there were no duplicated occurrences
+
 # 2.5. Remove flagged records --------------------------------------------------
 
 # Identify flagged records
 coordinate_flags <- clean_coordinates(x = clean_occurrences4,
-                                      lon = "decimalLongitude", 
+                                      lon = "decimalLongitude",
                                       lat = "decimalLatitude",
                                       species = "species",
                                       test = c("equal", "gbif", "zeros"))
@@ -44,6 +59,9 @@ summary(coordinate_flags) # no flagged records
 
 # 3. PREP DF FOR ANALYSIS ------------------------------------------------------
 
+# Check column names
+colnames(clean_occurrences4)
+
 # Remove unnecessary columns and add 1 more column
 clean_occurrences <- clean_occurrences4 |>
   select(gbifID, identifiedBy, basisOfRecord, occurrenceStatus,
@@ -51,52 +69,21 @@ clean_occurrences <- clean_occurrences4 |>
          locality, decimalLatitude, decimalLongitude, 
          coordinateUncertaintyInMeters, kingdom, phylum, class, order, family,
          genus, specificEpithet, speciesKey, species, organismQuantity,
-         occurrenceStatus, scientificName) |>
+         occurrenceStatus, scientificName, eventID, parentEventID, samplingEffort) |>
   mutate(period = case_when(year %in% c(2000:2005) ~ "2000.2005",
                             year %in% c(2006:2011) ~ "2006.2011",
                             year %in% c(2012:2018) ~ "2012.2018"))
 
 # 4. REMOVE RECORDS BASED ON COORDINATE UNCERTAINTY ----------------------------
 
-## 4.1. < 100m uncertainty -----------------------------------------------------
-
-# Remove records with coord uncertainty >100m
-clean_occurrences_100m <- clean_occurrences |>
-  filter(coordinateUncertaintyInMeters < 100 &
-           !is.na(coordinateUncertaintyInMeters))
-
-# Save cleaned occurrences
-write.csv(clean_occurrences_100m,
-          here("data", "derived_data", "clean_occurrences_100m.txt"))
-
-## 4.2. < 1km uncertainty ------------------------------------------------------
-
-# Remove records with coord uncertainty >1000m
-clean_occurrences_1km <- clean_occurrences |>
-  filter(coordinateUncertaintyInMeters < 1000 &
-           !is.na(coordinateUncertaintyInMeters))
-
-# Save cleaned occurrences
-write.csv(clean_occurrences_1km,
-          here("data", "derived_data", "clean_occurrences_1km.txt"))
-
-## 4.3. < 5km uncertainty ------------------------------------------------------
-
-# Remove records with coord uncertainty >5000m
-clean_occurrences_5km <- clean_occurrences |>
-  filter(coordinateUncertaintyInMeters < 5000 &
-           !is.na(coordinateUncertaintyInMeters))
-
-# Save cleaned occurrences
-write.csv(clean_occurrences_5km,
-          here("data", "derived_data", "clean_occurrences_5km.txt"))
-
-## 4.4. < 15km uncertainty ------------------------------------------------------
-
+# Only keep records with coordinate uncertainty lower than 15km (15000m)
 # Remove records with coord uncertainty >15000m
 clean_occurrences_15km <- clean_occurrences |>
   filter(coordinateUncertaintyInMeters < 15000 &
            !is.na(coordinateUncertaintyInMeters))
+
+# Check how many records are left in the cleaned df
+nrow(clean_occurrences_15km) # 25 196 761
 
 # Save cleaned occurrences
 write.csv(clean_occurrences_15km,
