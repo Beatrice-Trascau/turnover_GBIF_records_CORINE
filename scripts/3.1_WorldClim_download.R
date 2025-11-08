b4222dcfb9bae306b725dc44f3751bb4b7beba6d
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-11-08 18:34:04 +0100

    chore: rename script

--- scripts/3.1_WorldClim_download.R
+++ scripts/3.1_WorldClim_download.R
@@ -1,9 +1,17 @@
 ##----------------------------------------------------------------------------##
 # PAPER 2: CORINE LAND COVER CHANGES AND TURNOVER OF GBIF BIODIVERSITY RECORDS 
-# 3.1_CHELSA_download
-# This script contains code which donwlodads CHELSA bioclimatic variables for
-# use in downstream analyses
+# 3.1_WorldClim_download
+# This script contains code which downloads WorldClim minimum temperature,
+# maximum temperature and precipitation
 ##----------------------------------------------------------------------------##
+
+# 1. DOWNLOAD WORLDCLIM DATA ---------------------------------------------------
+
+# Historical WorldClim maximum temperature, minimum temperaute and precipitation were downloaded from https://worldclim.org/data/monthlywth.html on 08.11.2025 and uploaded to GoogleDrive
+# Donwload folders for eah period and variable
+
+
+
 
 # 1. LOAD NORWAY SHAPEFILE -----------------------------------------------------
 
@@ -12,8 +20,14 @@ norway_corine_projection <- vect(here("data", "derived_data",
                                       "reprojected_norway_shapefile", 
                                       "norway_corine_projection.shp"))
 
+# Convert to sf object
+norway_sf <- st_as_sf(norway_corine_projection)
+
 # Check CRS
-crs(norway_corine_projection, proj = TRUE)
+st_crs(norway_sf)$input
+
+# Check geometry type
+st_geometry_type(norway_sf, by_geometry = FALSE)
 
 # 3. DOWNLOAD CHELSA BIOCLIMATIC VARIABLES -------------------------------------
 
