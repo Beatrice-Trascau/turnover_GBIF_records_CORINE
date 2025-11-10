8b350e96ed92ead7f31200047487fb29df628b47
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-11-10 16:59:23 +0100

    feat: download WorldClim files from google drive

--- scripts/3.1_WorldClim_download.R
+++ scripts/3.1_WorldClim_download.R
@@ -7,37 +7,54 @@
 
 # 1. DOWNLOAD WORLDCLIM DATA ---------------------------------------------------
 
+# Define directory for the data
+worldclim_directory <- here("data", "raw_data", "worldclim")
+
 # Historical WorldClim maximum temperature, minimum temperaute and precipitation were downloaded from https://worldclim.org/data/monthlywth.html on 08.11.2025 and uploaded to GoogleDrive
-# Donwload folders for eah period and variable
+# Download folders for each period and variable
 
+# Authenticate on Google Drive
+drive_auth()
 
+# Find Worlclim folder on Google Drive
+worldclim_parent <- drive_find(pattern = "WorldClim", type = "folder") |>
+  filter(name == "WorldClim") |>
+  slice(1) # just in case it finds more than one folder - it shouldn't
+cat("Using folder:", worldclim_parent$name, "\n")
+cat("Folder ID:", worldclim_parent$id, "\n\n")
 
+# Get all the folders within the WorldClim folder
+worldclim_folders <- drive_ls(as_id(worldclim_parent$id), type = "folder")
 
-# 1. LOAD NORWAY SHAPEFILE -----------------------------------------------------
+# Check how many and which folders were found
+cat("Found", nrow(worldclim_folders), "folders to download:\n")
+print(worldclim_folders |> select(name))
 
-# Read in the reprojected shapefile
+# Download each folder - use function defined in the 0_setup.R script
+for(i in seq_len(nrow(worldclim_folders))){
+  folder_info <- worldclim_folders[i, ]
+  cat("\n", paste(rep("-", 60), collapse = ""), "\n")
+  cat("Processing folder", i, "of", nrow(worldclim_folders), ":", 
+      folder_info$name, "\n")
+  
+  download_drive_folder(folder_info$id, 
+                        folder_info$name, 
+                        worldclim_dir)
+}
+
+# 2. CROP AND MASK CLIMATIC VARIABLES TO NORWAY --------------------------------
+
+# Load Norway shapefile
 norway_corine_projection <- vect(here("data", "derived_data", 
                                       "reprojected_norway_shapefile", 
                                       "norway_corine_projection.shp"))
 
-# Convert to sf object
-norway_sf <- st_as_sf(norway_corine_projection)
-
 # Check CRS
-st_crs(norway_sf)$input
+st_crs(norway_corine_projection)$input
 
-# Check geometry type
-st_geometry_type(norway_sf, by_geometry = FALSE)
 
-# 3. DOWNLOAD CHELSA BIOCLIMATIC VARIABLES -------------------------------------
 
-## 3.1. Set up download parameters ---------------------------------------------
 
-# Define directory for the download
-chelsa_raw_directory <- here("data", "raw_data", "chelsa")
 
-# Define directory for the processed CHELSA fiels
-chelsa_derived_directory <- here("data", "derived_data", "chelsa")
 
-# Define bioclimatic variables to download
-bioclimatic_variables <- paste0("bio", 1:19)
+
