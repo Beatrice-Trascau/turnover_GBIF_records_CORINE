b4947e4d92339d449479928123951d253bbff7d6
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2026-02-03 18:20:42 +0100

    feat: extract citation for betapart package

--- scripts/8_packages.R
+++ scripts/8_packages.R
@@ -196,4 +196,41 @@ ris <- c("TY  - COMP",
 # Write .RIS reference to file
 writeLines(ris, "references/nlme.ris")
 
+## 2.4. BETAPART ------------------------------------------------------
+
+# Get citation info
+cit <- citation("betapart")
+
+# Extract authors
+authors <- paste(
+  sapply(cit$author, function(a) {
+    paste(a$family, paste(a$given, collapse = " "), sep = ", ")
+  }),
+  collapse = "\nAU  - "
+)
+
+# Extract year
+year <- cit$year
+
+# Extract title
+title <- cit$title
+
+# Extract version
+version <- as.character(packageVersion("betapart"))
+
+# Extract URL
+url <- cit$url
+
+# Build RIS entry
+ris <- c("TY  - COMP",
+         paste0("TI  - ", title),
+         paste0("AU  - ", authors),
+         paste0("PY  - ", year),
+         paste0("N1  - R package version ", version),
+         paste0("UR  - ", url),
+         "ER  -")
+
+# Write .RIS reference to file
+writeLines(ris, "references/betapart.ris")
+
 # END OF SCRIPT ----------------------------------------------------------------
